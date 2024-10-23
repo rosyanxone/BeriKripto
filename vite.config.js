a8af015f75e7a6f3ca2ee545ba022497985f1ca6
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2024-10-23 22:45:59 +0800

    install hostile

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "local.berikripto.me",
-    port: 80,
-  },
-})
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()],
+    server: {
+      host:
+        process.env.VITE_DEVELOPMENT == "local"
+          ? "localhost"
+          : "local.berikripto.me",
+      port:
+        process.env.VITE_DEVELOPMENT == "local"
+          ? 5173
+          : 80,
+    },
+  };
+});
